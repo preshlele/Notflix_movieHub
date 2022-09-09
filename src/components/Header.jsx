7c2d82a68b771eed5d95b32b89a90ed4a8e3fa4f
emmens4u <emmanuel.mensah@amalitech.org>
2022-09-09 20:23:43 +0800

    Searchbar implemented on Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,23 +11,29 @@ import {NavLink} from 'react-router-dom'
 const Header = () => {
 
   return (
-    <div className='bg-primary h-20 flex items-center px-20 justify-between'>
-        <div className='flex items-center text-white font-bold gap-10 ml-8 w-full z-[100]'>
-          <NavLink to="/"><h1 className='text-red-600 font-bold cursor-pointer text-2xl'>NUTFLIX</h1></NavLink>  
-        </div>
-        <div className='w-full mr-[460px] flex px-8 gap-3 justify-between'>
-        <MoviesMenu/>
-        <TvShowsMenu/>
-        <PopularMenu/> 
-        </div>
-        
+    <div className="bg-primary h-20 flex items-center px-20 justify-between">
+      <div className="flex items-center text-white font-bold gap-10 ml-8 w-full z-[100]">
+        <NavLink to="/">
+          <h1 className="text-red-600 font-bold cursor-pointer text-2xl">
+            NUTFLIX
+          </h1>
+        </NavLink>
+      </div>
+      <div className="w-full mr-[460px] flex px-8 gap-3 justify-between">
+        <MoviesMenu />
+        <TvShowsMenu />
+        <PopularMenu />
+      </div>
 
-        <div className="text-white flex items-center gap-5 font-bold">
-            <button className='bg-red-600 px-6 py-2 rounded cursor-pointer text-white'>Login</button>
-            <SearchBar/>
-        </div>
+      <div className="text-white flex items-center gap-5 font-bold">
+        <NavLink to="/search-results">
+          <button className='btn inline-block px-6 py-2 border-2 border-white text-white font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out" type="button'>
+            Search
+          </button>
+        </NavLink>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Header
